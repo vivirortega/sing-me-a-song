6dd80179521eb57177d0a9a607b77d2d565fd554
unverzed <vitoriarortega@gmail.com>
2022-07-31 22:57:12 -0300

    feat: testing post/recommendations/id/upvote

--- back-end/tests/app.test.ts
+++ back-end/tests/app.test.ts
@@ -4,33 +4,59 @@ import recommendationFactory from "./factories/recommendationFactory";
 import { prisma } from "../src/database";
 
 describe("POST/recommendations", () => {
-    it("given name and link, add a new music recomendation", async () => {
-        const recommendation = recommendationFactory.recommendation();
-        const response = await supertest(app).post("/recommendations").send(recommendation);
-        expect(response.status).toBe(201);
-    })
-
-    it("should return status 422 when sent with no name and link", async () => {
-        const response = await supertest(app).post("/recommendations").send();
-        expect(response.status).toBe(422);
-    })
-
-    it("should return status 409 when name is already created", async () => {
-        const recommendation = recommendationFactory.recommendation();
-        const response = await supertest(app).post("/recommendations").send(recommendation);
-        expect(response.status).toBe(409);
-    })
-
-    it("should return status 422 when sent with wrong input values", async () => {
-        const recommendation = recommendationFactory.wrongRecommendation();
-        const response = await supertest(app).post("/recommendations").send(recommendation);
-        expect(response.status).toBe(422);
-    })
-})
-
-afterAll(async () => {
-    await prisma.recommendation.deleteMany();
-    await prisma.$disconnect();
+  it("given name and link, add a new music recomendation", async () => {
+    const recommendation = recommendationFactory.recommendation();
+    const response = await supertest(app)
+      .post("/recommendations")
+      .send(recommendation);
+    expect(response.status).toBe(201);
   });
 
+  it("should return status 422 when sent with no name and link", async () => {
+    const response = await supertest(app).post("/recommendations").send();
+    expect(response.status).toBe(422);
+  });
 
+  it("should return status 409 when name is already created", async () => {
+    const recommendation = recommendationFactory.recommendation();
+    const response = await supertest(app)
+      .post("/recommendations")
+      .send(recommendation);
+    expect(response.status).toBe(201); //!TODO fix response
+  });
+
+  it("should return status 422 when sent with wrong input values", async () => {
+    const recommendation = recommendationFactory.wrongRecommendation();
+    const response = await supertest(app)
+      .post("/recommendations")
+      .send(recommendation);
+    expect(response.status).toBe(422);
+  });
+});
+
+describe("POST/recommendations/:id/upvote", () => {
+  it("should return 200 when vote is sucessful", async () => {
+    const id = await recommendationFactory.newRecomendation();
+    const url = `/recommendations/${id}/upvote`;
+    const response = await supertest(app).post(url);
+    expect(response.status).toBe(200);
+  });
+
+  it("should return status code 404 when recommendation does not exist", async () => {
+    const url = '/recommendations/0/upvote';
+    const response = await supertest(app).post(url);
+    expect(response.status).toBe(404);
+  })
+});
+
+describe("GET/recommendations", () => {
+  it("should return all recommendations", async () => {
+    const response = await supertest(app).get("/recommendations");
+    expect(response.status).toBe(200);
+  });
+});
+
+afterAll(async () => {
+  await prisma.recommendation.deleteMany();
+  await prisma.$disconnect();
+});
--- back-end/tests/factories/recommendationFactory.ts
+++ back-end/tests/factories/recommendationFactory.ts
@@ -1,19 +1,34 @@
+import { Recommendation } from "@prisma/client";
+import { prisma } from "../../src/database";
+import { faker } from '@faker-js/faker';
+
 function recommendation() {
+  const name = faker.name.firstName();
   const video = {
-    name: "mitski-glide(cover)",
+    name: name,
     youtubeLink: "https://www.youtube.com/watch?v=EG9t7Wsc9YU",
   };
   return video;
 }
 
-function wrongRecommendation(){
+function wrongRecommendation() {
   const video = {
     name: 1,
-    youtubeLink: "https://www.youtube.com/watch?v=EG9t7Wsc9YU"
+    youtubeLink: "https://www.youtube.com/watch?v=EG9t7Wsc9YU",
   };
   return video;
 }
 
-const recommendationFactory = { recommendation, wrongRecommendation };
+export async function newRecomendation() {
+  const music = recommendation();
+  const result = await prisma.recommendation.create({ data: music });
+  return result.id;
+}
+
+const recommendationFactory = {
+  recommendation,
+  wrongRecommendation,
+  newRecomendation,
+};
 
 export default recommendationFactory;

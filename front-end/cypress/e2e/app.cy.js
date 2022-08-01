b67a5c519b101f59f7fde8791b755e45d56a6e73
unverzed <vitoriarortega@gmail.com>
2022-08-01 14:54:00 -0300

    fix: should remove a recommendation when score is -5

--- front-end/cypress/e2e/app.cy.js
+++ front-end/cypress/e2e/app.cy.js
@@ -18,30 +18,38 @@ describe("post/recommendation", () => {
     cy.end();
   });
 
-  it('should return alert when name is already created', () => {
+  it("should return alert when name is already created", () => {
     cy.visit(`${url}/`);
     cy.get("#name").type(recommendation.name);
     cy.get("#youtube-link").type(recommendation.youtubeLink);
     cy.get("#submit").click();
-    cy.on('window:alert', (text) => {
-      expect(text).to.contains('Error creating recommendation!');
+    cy.on("window:alert", (text) => {
+      expect(text).to.contains("Error creating recommendation!");
     });
     cy.end();
   });
 });
 
 describe("post/recommendation/id/upvote", () => {
-    it("should vote in a recommendation", () => {
-      cy.visit(`${url}/`);
-      cy.get("#arrow-up").click();
-      cy.end();
-    });
+  it("should vote in a recommendation", () => {
+    cy.visit(`${url}/`);
+    cy.get("#arrow-up").click();
+    cy.end();
+  });
 });
 
 describe("post/recommendation/id/downvote", () => {
-    it("should vote in a recommendation", () => {
-      cy.visit(`${url}/`);
+  it("should vote in a recommendation", () => {
+    cy.visit(`${url}/`);
+    cy.get("#arrow-down").click();
+    cy.end();
+  });
+
+  it("should remove a recommendation when score is -5", () => {
+    cy.visit(`${url}/`);
+    for (let i = 0; i <= 5; i++) {
       cy.get("#arrow-down").click();
-      cy.end();
-    });
+    }
+    cy.end();
+  });
 });

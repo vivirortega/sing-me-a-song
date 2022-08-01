b6d0a38dbeb1c47c8be83f0eb7b8782c3b6813fd
unverzed <vitoriarortega@gmail.com>
2022-08-01 16:12:14 -0300

    feat: unit tests post/downvote when score is -5

--- back-end/tests/unit/recommendationService.test.ts
+++ back-end/tests/unit/recommendationService.test.ts
@@ -36,3 +36,58 @@ describe("post/recommendations", () => {
     });
   });
 });
+
+describe("post/recommendations/:id/upvote", () => {
+  it("should vote if sucessfull", async () => {
+    jest
+      .spyOn(recommendationRepository, "find")
+      .mockResolvedValueOnce(recommendation);
+    jest
+      .spyOn(recommendationRepository, "updateScore")
+      .mockResolvedValueOnce({ ...recommendation, score: 6 });
+    await recommendationService.upvote(recommendation.id);
+    expect(recommendationRepository.updateScore).toBeCalledTimes(1);
+  });
+
+  it("should throw not found if recommendation does not exist", async () => {
+    jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(null);
+    expect(recommendationService.upvote(1)).rejects.toEqual({
+      type: "not_found",
+      message: "",
+    });
+  });
+});
+
+describe("post/recommendations/:id/downvote", () => {
+  it("should downvote if sucessfull", async () => {
+    jest
+      .spyOn(recommendationRepository, "find")
+      .mockResolvedValueOnce(recommendation);
+    const update = jest
+      .spyOn(recommendationRepository, "updateScore")
+      .mockResolvedValueOnce({ ...recommendation, score: -3 });
+
+    await recommendationService.downvote(recommendation.id);
+
+    expect(update).toBeCalledTimes(2);
+  });
+
+  it("should throw not found if recommendation does not exist", async () => {
+    jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(null);
+    expect(recommendationService.upvote(1)).rejects.toEqual({
+      type: "not_found",
+      message: "",
+    });
+  });
+
+  it("should remove a recommendation when score is -5", async () => {
+    const recommendationBellow = {
+        ...recommendation, score: -5
+    }
+    jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(recommendationBellow);
+    jest.spyOn(recommendationRepository, "updateScore").mockResolvedValueOnce({...recommendation, score: -6});
+    jest.spyOn(recommendationRepository, "remove").mockResolvedValueOnce();
+    await recommendationService.downvote(recommendation.id);
+    expect(recommendationRepository.remove).toBeCalledTimes(1);
+  });
+});
